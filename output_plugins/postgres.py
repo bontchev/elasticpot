ff5ab33d6badedf3ad5204bb0e2fd746d86a3682
Vesselin Bontchev <vbontchev@yahoo.com>
2020-07-04 19:20:24 +0300

    Fixed numerous bugs.

--- output_plugins/postgres.py
+++ output_plugins/postgres.py
@@ -14,9 +14,9 @@ from twisted.python import log
 class Output(output.Output):
 
     def start(self):
-        host = CONFIG.get('output_postgres', 'host')
+        host = CONFIG.get('output_postgres', 'host', fallback='localhost')
         port = CONFIG.getint('output_postgres', 'port', fallback=5432)
-        username = CONFIG.get('output_postgres', 'username')
+        username = CONFIG.get('output_postgres', 'username', fallback='elasticpot')
         password = CONFIG.get('output_postgres', 'password')
         database = CONFIG.get('output_postgres', 'database', fallback='elasticpot')
         self.debug = CONFIG.getboolean('output_postgres', 'debug', fallback=False)
@@ -33,7 +33,7 @@ class Output(output.Output):
         except Exception as e:
             log.msg(e)
 
-        self.curr = conn.cursor()
+        self.cur = self.conn.cursor()
 
         if self.geoip:
             geoipdb_city_path = CONFIG.get('output_postgres', 'geoip_citydb', fallback='data/GeoLite2-City.mmdb')
@@ -50,8 +50,8 @@ class Output(output.Output):
                 log.msg('Failed to open ASN GeoIP database {}'.format(geoipdb_asn_path))
 
     def stop(self):
-        cur.close()
-        conn.close()
+        self.cur.close()
+        self.conn.close()
         if self.geoip:
             if self.reader_city is not None:
                 self.reader_city.close()
@@ -61,32 +61,26 @@ class Output(output.Output):
     def write(self, event):
         self.connect_event(event)
 
-    def simple_query(self, sql, args):
+    def simple_query(self, sql, args, returns_value=True):
         if self.debug:
-            if len(args):
-                log.msg('output_postgres: postgres query: {} {}'.format(sql, repr(args)))
-            else:
-                log.msg('output_postgres: postgres query: {}'.format(sql))
+            log.msg('output_postgres: postgres query: {} {}'.format(sql, repr(args)))
+        result = None
         try:
-            if len(args):
-                self.cur.execute(sql, args)
-            else:
-                self.cur.execute(sql)
-            result = self.cur.fetchmany()
+            self.cur.execute(sql, args)
+            if returns_value:
+                result = self.cur.fetchone()
         except Exception as e:
             log.msg('output_postgres: postgres Error: {}'.format(e))
-            result = None
         return result
 
     def get_id(self, table, column, entry):
         r = self.simple_query("SELECT id FROM {} WHERE {} = %s".format(table, column), (entry, ))
         if r:
-            id = r[0][0]
+            id = int(r[0])
         else:
-            self.simple_query("INSERT INTO {} ({}) VALUES (%s)".format(table, column), (entry, ))
-            r = self.simple_query('SELECT LAST_INSERT_ROWID()', ())
+            r = self.simple_query("INSERT INTO {} ({}) VALUES (%s) RETURNING id".format(table, column), (entry, ))
             if r:
-                id = int(r[0][0])
+                id = int(r[0])
             else:
                 id = 0
         return id
@@ -96,13 +90,12 @@ class Output(output.Output):
         shasum = sha256(sc).hexdigest()
         r = self.simple_query("SELECT id FROM {} WHERE inputhash = %s".format(table), (shasum, ))
         if r:
-            id = int(r[0][0])
+            id = int(r[0])
         else:
-            self.simple_query("INSERT INTO {} (input, inputhash) VALUES (%s, %s)".format(table),
-                              (sc.decode('utf-8').encode('unicode_escape'), shasum, ))
-            r = self.simple_query('SELECT LAST_INSERT_ROWID()', ())
+            r = self.simple_query("INSERT INTO {} (input, inputhash) VALUES (%s, %s) RETURNING id".format(table),
+                                  (sc.decode('utf-8').encode('unicode_escape'), shasum, ))
             if r:
-                id = int(r[0][0])
+                id = int(r[0])
             else:
                 id = 0
         return id
@@ -115,46 +108,42 @@ class Output(output.Output):
         if 'payload' in event:
             payload_id = self.get_hashed_id('payloads', event['payload'])
         else:
-            payload_id = 'NULL'
+            payload_id = 0
         if 'user_agent' in event:
             agent_id = self.get_id('user_agents', 'user_agent', event['user_agent'])
         else:
-            agent_id = 'NULL'
+            agent_id = 0
         if 'content_type' in event:
             content_id = self.get_id('content_types', 'content_type', event['content_type'])
         else:
-            content_id = 'NULL'
+            content_id = 0
         if 'accept_language' in event:
             language_id = self.get_id('accept_languages', 'accept_language', event['accept_language'])
         else:
-            language_id = 'NULL'
+            language_id = 0
         sensor_id = self.get_id('sensors', 'name', event['sensor'])
 
         self.simple_query("""
             INSERT INTO connections (
-                timestamp, ip, remote_port, request, url,
+                time_stamp, ip, remote_port, request, url,
                 payload, message, user_agent, content_type,
                 accept_language, local_host, local_port, sensor)
-            VALUES (DATETIME(%s, 'unixepoch', 'localtime'), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            VALUES (to_timestamp(%s), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
             """,
             (event['unixtime'], remote_ip, event['src_port'], event['request'], path_id, payload_id,
-            message_id, agent_id, content_id, language_id, event['dst_ip'], event['dst_port'], sensor_id, ))
+            message_id, agent_id, content_id, language_id, event['dst_ip'], event['dst_port'], sensor_id, ), False)
 
         if self.geoip:
             country, country_code, city, org, asn_num = geolocate(remote_ip, self.reader_city, self.reader_asn)
-            result = self.simple_query("""
+            self.simple_query("""
                 INSERT INTO geolocation (ip, country_name, country_iso_code, city_name, org, org_asn)
                 VALUES (%s, %s, %s, %s, %s, %s)
+                ON CONFLICT (ip) DO UPDATE SET
+                    country_name = %s,
+                    country_iso_code = %s,
+                    city_name = %s,
+                    org = %s,
+                    org_asn = %s
                 """,
-                (remote_ip, country, country_code, city, org, asn_num, ))
-            if result is None:
-                self.simple_query("""
-                    UPDATE geolocation SET
-                        country_name = %s,
-                        country_iso_code = %s,
-                        city_name = %s,
-                        org = %s,
-                        org_asn = %s
-                    WHERE ip = %s
-                    """,
-                    (country, country_code, city, org, asn_num, remote_ip, ))
+                (remote_ip, country, country_code, city, org, asn_num, country, country_code, city, org, asn_num, ), False)
+        self.conn.commit()
